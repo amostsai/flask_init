8b78fd4f571d13f08c24e3e118f8eabaea4831d0
amos tsai <amos.tsai@gmail.com>
2019-06-01 07:14:13 +0800

    modify: app.py範例在清楚一些

--- app.py
+++ app.py
@@ -1,38 +1,39 @@
-#from flask import Flask
-#
-#app = Flask(__name__)
-#@app.route('/')
-#def hello_world():
-#    return 'Hello world!'
-#
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0')
-
-
-
-#from flask import Flask, render_template
-#
-#app = Flask(__name__)
-#
-#@app.route('/', methods=['GET', 'POST'])
-#def index():
-#    return render_template('index.html')
-#
-#if __name__ == '__main__':
-#    app.run()
-
-
-from datetime import datetime
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
+from datetime import datetime
+
+# 第一個參數是應用程式模組(module)或套件(package)的名稱，這樣flask才知道要去哪裡找範本、靜態檔案...
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello world!'
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
+
+
+# =================================================================
+# app = Flask(__name__)
+
+# @app.route('/', methods=['GET', 'POST'])  # GET, POST方法都會由index這個函數來執行
+# def index():
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.run()
+
+
+# =================================================================
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'mysql'
 app.config['MYSQL_USER'] = 'hccu'
 app.config['MYSQL_PASSWORD'] = 'hccu'
 app.config['MYSQL_DB'] = 'hccu'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(app.config['MYSQL_USER'], app.config['MYSQL_PASSWORD'], app.config['MYSQL_HOST'], app.config['MYSQL_DB'])
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
+    app.config['MYSQL_USER'], app.config['MYSQL_PASSWORD'], app.config['MYSQL_HOST'], app.config['MYSQL_DB'])
 print(app.config['SQLALCHEMY_DATABASE_URI'])
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://hccu:hccu@mysql/hccu'
 #app.config['SQLALCHEMY_DATABASE_URI'] = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(DIALECT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
@@ -49,7 +50,8 @@ def index():
         lastName = details['lname']
         createDateTime = datetime.now().strftime('%Y/%m/%d %H:%M:%S.%f')
 
-        sql = text('INSERT INTO MyUsers(firstName, lastName, createDateTime) VALUES ("%s", "%s", "%s")' % (firstName, lastName, createDateTime))
+        sql = text('INSERT INTO MyUsers(firstName, lastName, createDateTime) VALUES ("%s", "%s", "%s")' % (
+            firstName, lastName, createDateTime))
         db.engine.execute(sql)
         return 'success'
     return render_template('index.html')
