7b220315091543151f0398d059cfc6eeb9bea69f
amos tsai <amos.tsai@gmail.com>
2019-06-01 15:00:45 +0800

    new: 實做flask-sqlalchemy ORM範例

--- app.py
+++ app.py
@@ -11,17 +11,20 @@ db = SQLAlchemy()
 db.init_app(app)
 
 
-# class User(db.Model):
-#     id = db.Column(db.Integer(), primary_key=True)
-#     username = db.Column(db.String(255))
-#     password = db.Column(db.String(255))
-#     lastLoginDateTime = db.Column(db.DateTime)
+class Users(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    lastLoginDateTime = db.Column(db.DateTime)
 
-#     def __init__(self, username):
-#         self.username = username
+    def __init__(self, username, password, lastLoginDateTime):
+        self.username = username
+        self.password = password
+        self.lastLoginDateTime = lastLoginDateTime
 
-#     def __repr__(self):
-#         return "<User '{}'>".format(self.username)
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,10 +35,14 @@ def index():
         password = details['password']
         lastLoginDateTime = datetime.now().strftime('%Y/%m/%d %H:%M:%S.%f')
 
-        sql = text('INSERT INTO Users(username, password, lastLoginDateTime) VALUES ("%s", "%s", "%s")' % (
-            username, password, lastLoginDateTime))
-        print(sql)
-        db.engine.execute(sql)
+        user = Users(username=username, password=password,
+                     lastLoginDateTime=lastLoginDateTime)
+        db.session.add(user)
+        db.session.commit()
+        # sql = text('INSERT INTO Users(username, password, lastLoginDateTime) VALUES ("%s", "%s", "%s")' % (
+        #     username, password, lastLoginDateTime))
+        # print(sql)
+        # db.engine.execute(sql)
         return 'success'
     return render_template('index.html')
 
