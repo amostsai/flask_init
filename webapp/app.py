7cd2caadc8163cc5bacb2bf366c3e2acb0d77f04
amos tsai <amos.tsai@gmail.com>
2019-07-04 13:05:53 +0800

    new: 新增celery, redis架構

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
-from extensions import debug_toolbar
+from flask import url_for
+import celery.states as states
 
+from worker import celery
 from blueprints.news import news
+from extensions import debug_toolbar
 
 
 def create_app(mode):
@@ -31,3 +34,20 @@ def extensions(app):
 # default to prod config
 env = os.environ.get('FLASK_ENV', 'prod')
 app = create_app('config.%sConfig' % env.capitalize())
+
+
+# test celery
+@app.route('/add/<int:param1>/<int:param2>')
+def add(param1: int, param2: int) -> str:
+    task = celery.send_task('tasks.cal.add', args=[param1, param2], kwargs={})
+    response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+    return response
+
+
+@app.route('/check/<string:task_id>')
+def check_task(task_id: str) -> str:
+    res = celery.AsyncResult(task_id)
+    if res.state == states.PENDING:
+        return res.state
+    else:
+        return str(res.result)
